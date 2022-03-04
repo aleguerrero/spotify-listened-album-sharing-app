109eafa72905fe18fb0efb12338ae021ee20180e
[aleguerrero] <[alegue17@gmail.com]>
2022-03-03 23:12:52 -0600

    authentication now working

--- authorizationCode.py
+++ authorizationCode.py
@@ -5,44 +5,25 @@ import requests
 def authorize():
     clientId = "<clientid>"
     secret = "<secret>"
-    redirectUri = "http://localhost:8888/"
-    
-    # URLS
-    AUTH_URL = 'https://accounts.spotify.com/authorize'
-    TOKEN_URL = 'https://accounts.spotify.com/api/token'
-    BASE_URL = 'https://api.spotify.com/v1/'
 
+    clientIdSecret = clientId + ':' + secret
+    clientIdSecret_bytes = clientIdSecret.encode('ascii')
+    base64_bytes = base64.b64encode(clientIdSecret_bytes)
+    base64_clientIdSecret = base64_bytes.decode('ascii')
 
-    # Make a request to the /authorize endpoint to get an authorization code
-    auth_code = requests.get(AUTH_URL, {
-        'client_id': clientId,
-        'response_type': 'code',
-        'redirect_uri': 'https://open.spotify.com/collection/playlists',
-        'scope': 'playlist-modify-private',
-    })
-    print(auth_code)
+    url = 'https://accounts.spotify.com/api/token'
 
-    auth_header = base64.urlsafe_b64encode((clientId + ':' + secret).encode('ascii'))
     headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic %s' % auth_header.decode('ascii')
+        'Authorization': 'Basic ' + base64_clientIdSecret
     }
 
-    payload = {
-        'grant_type': 'authorization_code',
-        'code': auth_code,
-        'redirect_uri': 'https://open.spotify.com/collection/playlists',
-        #'client_id': CLIENT_ID,
-        #'client_secret': CLIENT_SECRET,
+    form = {
+        'grant_type': 'client_credentials'
     }
 
-    # Make a request to the /token endpoint to get an access token
-    access_token_request = requests.post(url=TOKEN_URL, data=payload, headers=headers)
+    jsonResult = requests.post(url, data=form, json=True, headers=headers)
 
-    # convert the response to JSON
-    access_token_response_data = access_token_request.json()
+    if jsonResult.ok:
+        print (jsonResult.json())
 
-    print(access_token_response_data)
-
-    # save the access token
-    access_token = access_token_response_data['access_token']
+print(authorize())
