99acdf7875c54a9880fddfe35ac3e84ff2284499
Guerrero Aguilar, Alejandro <a.guerrero.aguilar@accenture.com>
2022-03-26 01:09:44 -0600

    checking redirecturi

--- authorizationCode.py
+++ authorizationCode.py
@@ -1,52 +1,68 @@
 from asyncio import base_events
 import base64
 import requests
-from flask import Flask
+from flask import Flask, redirect, request
 import os
+import string    
+import random
+
+# variables
+clientId = os.environ.get('ClientId')
+secret = os.environ.get('Secret')
+redirectUri = os.environ.get('redirectUri')
+
+# generates random string for authorization
+def randomStringGenerator(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
 # test flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/login", methods=['GET'])
+def login():
+    state = randomStringGenerator(16)
+    scope = 'user-read-recently-played'
 
+    return redirect(f"https://accounts.spotify.com/authorize?response_type=code&client_id={clientId}&scope={scope}&redirect_uri={redirectUri}&state={state}")
+
+@app.route("/callback", methods=['GET'])
 def authorize():
-    clientId = os.environ.get('ClientId')
-    secret = os.environ.get('Secret')
 
-    clientIdSecret = clientId + ':' + secret
-    clientIdSecret_bytes = clientIdSecret.encode('ascii')
-    base64_bytes = base64.b64encode(clientIdSecret_bytes)
-    base64_clientIdSecret = base64_bytes.decode('ascii')
-
-    url = 'https://accounts.spotify.com/api/token'
-
-    headers = {
-        'Authorization': 'Basic ' + base64_clientIdSecret
-    }
-
-    form = {
-        'grant_type': 'client_credentials'
-    }
-
-    jsonResult = requests.post(url, data=form, json=True, headers=headers)
-
-    if jsonResult.ok:
-        response = jsonResult.json()
-        return response['access_token']
-
-def testApi(authentication):
-    url = 'https://api.spotify.com/v1/me/player/recently-played'
-
-    headers = {
-        'Authorization': 'Bearer ' + str(authentication),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-
-    jsonResult = requests.get(url, json=True, headers=headers)
+    code = request.args.get('code')
+    state = request.args.get('state')
     
-    print(jsonResult.json())
+    if state:
+        clientIdSecret = clientId + ':' + secret
+        clientIdSecret_bytes = clientIdSecret.encode('ascii')
+        base64_bytes = base64.b64encode(clientIdSecret_bytes)
+        base64_clientIdSecret = base64_bytes.decode('ascii')
 
-testApi(authorize())
+        url = 'https://accounts.spotify.com/api/token'
+
+        headers = {
+            'Authorization': 'Basic ' + base64_clientIdSecret
+        }
+
+        form = {
+            'code': code,
+            'redirect_uri': redirectUri,
+            'grant_type': 'authorization_code'
+        }
+
+        jsonResult = requests.post(url, data=form, json=True, headers=headers)
+
+        if jsonResult.ok:
+            response = jsonResult.json()
+            accessToken = response['access_token']
+
+            url = 'https://api.spotify.com/v1/me/player/recently-played'
+
+            headers = {
+                'Authorization': 'Bearer ' + str(accessToken)
+            }
+
+            recentPlayed = requests.get(url, json=True, headers=headers)
+
+            if recentPlayed.ok:
+                return recentPlayed 
