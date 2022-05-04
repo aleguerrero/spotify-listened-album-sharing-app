f1d4b827bc84d7934705783ac036cd75bb6e6b1d
Alejandro Guerrero <alegue17@gmail.com>
2022-05-04 00:21:51 -0600

    added ids for songs and albums, also figuring threads in python

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from asyncio import base_events
 import base64
 from crypt import methods
+import threading
 import requests
 from flask import Flask, redirect, request
 import os
 import string    
 import random
 import json
+import time
+from celery import Celery
 
 # variables
 clientId = os.environ.get('ClientId')
 secret = os.environ.get('Secret')
 redirectUri = os.environ.get('redirectUri')
-# accessToken = ''
 
 # generates random string for authorization
 def randomStringGenerator(length):
@@ -20,16 +22,46 @@ def randomStringGenerator(length):
     return ''.join(random.choice(letters) for i in range(length))
 
 class Song:
-    def __init__(self, song, trackNumber, artist, album):
+    def __init__(self, songId, song, trackNumber, artist, albumId, album):
+        self.songId = songId
         self.song = song
         self.trackNumber = trackNumber
         self.artist = artist
+        self.albumId = albumId
         self.album = album
     
+class Album:
+    def __init__(self, albumId, albumName, artist, songs) -> None:
+        self.albumId = albumId
+        self.albumName = albumName
+        self.artist = artist
+        self.songs = songs
 
+# celery
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
 
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(app)
 
+# Gets link to redirect the callback and get a token
 @app.route("/login", methods=['GET'])
 def login():
     state = randomStringGenerator(16)
@@ -38,6 +70,7 @@ def login():
     authorizeUrl = f"https://accounts.spotify.com/authorize?response_type=code&client_id={clientId}&scope={scope}&redirect_uri={redirectUri}&state={state}"
     return redirect(authorizeUrl)
 
+# Gets the token for authorizing requests 
 @app.route("/callback", methods=['GET'])
 def authorize():
     code = request.args.get('code')
@@ -64,10 +97,11 @@ def authorize():
         if jsonResult.ok:
             response = jsonResult.json()
             os.environ['accessToken'] = response['access_token']
-            return redirect('/')
+            return redirect('/getRecentlyPlayedTracks')
 
-@app.route("/", methods=['GET'])
-def index():
+# Gets the latest 20 songs listened
+@app.route("/getRecentlyPlayedTracks", methods=['GET'])
+def getRecentlyPlayedTracks():
     if os.environ.get('accessToken') == None:
         return redirect('/login')
     else:
@@ -85,16 +119,50 @@ def index():
             recentPlayedResponse = recentPlayed.json()
             tracks = {}
             for item in range(len(recentPlayedResponse['items'])):
+                songId = recentPlayedResponse['items'][item]['track']['id']
                 songName = recentPlayedResponse['items'][item]['track']['name']
                 trackNumber = recentPlayedResponse['items'][item]['track']['track_number']
                 artist = recentPlayedResponse['items'][item]['track']['album']['artists'][0]['name']
                 album = recentPlayedResponse['items'][item]['track']['album']['name']
+                albumId = recentPlayedResponse['items'][item]['track']['album']['id']
 
-            # add class here
+                # add class here
+                song = Song(songId, songName, trackNumber, artist, albumId, album)
 
-            jsonReturned = json.dumps(recentPlayedResponse)
+                tracks[item] = song.__dict__
+            
+            jsonReturned = json.dumps(tracks)
             return json.loads(jsonReturned)
 
+# get the info of the album by id
+@app.route("/getAlbum/<string:albumId>", methods=['GET'])
+def getAlbum(albumId):
+    if os.environ.get('accessToken') == None:
+        return redirect('/login')
+    else:
+        accessToken = os.environ.get('accessToken')
+        
+        url = f'https://api.spotify.com/v1/albums/{albumId}/tracks'
+
+        headers = {
+            'Authorization': 'Bearer ' + str(accessToken)
+        }
+
+        albums = requests.get(url, headers=headers)
+
+        if albums.ok:
+            albumResponse = albums.json()
+            jsonReturned = json.dumps(albumResponse)
+            return json.loads(jsonReturned)
+
+@celery.task()
+def timer():
+    try:
+        time.sleep(10)
+        return redirect('/getRecentlyPlayedTracks')
+    except Exception as e:
+        return e
+    
 
 @app.route("/refresh_token")
 def refreshToken():
@@ -124,3 +192,8 @@ def encoding(clientId, secret):
     base64_bytes = base64.b64encode(clientIdSecret_bytes)
     base64_clientIdSecret = base64_bytes.decode('ascii')
     return base64_clientIdSecret
+
+# def requestFunction(url, headers, form, method):
+#     if method == 'GET':
+#         requests.get()
+#     elif method == 'POST':
